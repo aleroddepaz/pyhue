c6476c67e7db187aebcf4fbff5798a2129bf8d78
Alejandro <alexrdp90@gmail.com>
2013-06-03 16:32:59 +0200

    Fixed bug for group 0

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ See the complete documentation of the Philips Hue personal lighting system on ht
 
 setup(
     name = 'pyhue',
-    version = '0.7',
+    version = '0.7.1',
     license = 'GPLv3',
     description = 'Python library for the Philips Hue personal lighting system',
     long_description = long_description,
